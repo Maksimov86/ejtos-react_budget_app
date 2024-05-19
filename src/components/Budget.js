9a0d8e24bb6f9590c89e00aa03335e9a4d730c56
Maksimov86 <115940258+Maksimov86@users.noreply.github.com>
2024-05-19 12:45:29 +0700

    Update Budget.js

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,16 +2,25 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget, dispath } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
     const handleBudgetChange = (event) => {
-        setNewBudget(event.target.value);
+        const value = event.target.value;
+        if (value > 20000) {
+            alert("Бюджет не может превышать 20,000");
+        } else {
+            setNewBudget(value);
+            dispath({
+                type: 'SET_BUDGET',
+                payload: parseInt(value),
+            })
+        }
     }
     return (
-<div className='alert alert-secondary'>
-<span>Budget: £{budget}</span>
-<input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
-</div>
+        <div className='alert alert-secondary'>
+            <span>Budget: £{budget}</span>
+            <input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
+        </div>
     );
 };
 export default Budget;
